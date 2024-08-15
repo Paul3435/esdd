3341ae515ed38fd73ad8fbdb9177743cc1324e53
Paul <paul.borjesson.sesma@gmail.com>
2024-08-16 01:07:47 +0200

    /send endpoint is able to send emails now

--- server/server.go
+++ server/server.go
@@ -6,25 +6,56 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/Paul3435/esdd/pkg/email"
 )
 
-func landingPageHandler(w http.ResponseWriter, r *http.Request) {
-	html, err := os.ReadFile("./landingPage.html")
-	if err != nil {
-		http.Error(w, "Could not read HTML file", http.StatusInternalServerError)
-		return
-	}
+type MailContent struct {
+	Email   string
+	Subject string
+	Body    string
+}
 
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, string(html))
+func landingPageHandler(w http.ResponseWriter, r *http.Request) {
+	renderHTML(w, "landingPage.html")
 }
 
 func handleSendEmail(w http.ResponseWriter, r *http.Request) {
+	//Parse the form
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Could not parse form", http.StatusBadRequest)
+		return
+	}
 
+	//Render HTML
+	renderHTML(w, "sendForm.html")
+
+	//Gather data from the form in the POST req
+	mail := &MailContent{
+		Email:   r.PostFormValue("to"),
+		Subject: r.PostFormValue("subject"),
+		Body:    r.PostFormValue("body"),
+	}
+
+	emailServiceManager := email.NewEmailServiceManager(email.NewSendGrid("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"), email.NewMailgunService("02057f3a67aec395a2efd2e70426f144-911539ec-96815074"))
+	emailServiceManager.SendEmail(mail.Subject, mail.Email, mail.Body)
 }
 
 func Start() {
 	http.HandleFunc("/", landingPageHandler)
 	http.HandleFunc("/send", handleSendEmail)
 	http.ListenAndServe(":8080", nil)
+}
+
+func renderHTML(w http.ResponseWriter, htmlLocation string) error {
+	html, err := os.ReadFile(htmlLocation)
+	if err != nil {
+		http.Error(w, "Could not read HTML file", http.StatusInternalServerError)
+		return err
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, string(html))
+	return nil
 }
