6469ab7b22b8a63c031c8380681849d7a428c961
Paul <paul.borjesson.sesma@gmail.com>
2024-08-15 22:23:36 +0200

    remove obsolete error return

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,13 @@
 package main
 
-import "github.com/Paul3435/esdd/pkg/email"
-
 func main() {
-	sendGridAPIKey := "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"
-	sendGrid := email.NewSendGrid(sendGridAPIKey)
+	// sendGridAPIKey := "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"
+	// sendGrid := email.NewSendGrid(sendGridAPIKey)
 
-	sendGrid.SendEmail("Test Subject", "paul.borjesson.sesma3435@gmail.com", "This is a test email body.")
+	// sendGrid.SendEmail("Test Subject", "paul.borjesson.sesma3435@gmail.com", "This is a test email body.")
 
-	mailGunAPIKey := "02057f3a67aec395a2efd2e70426f144-911539ec-96815074"
-	mailGun := email.NewMailgunService(mailGunAPIKey)
+	// mailGunAPIKey := "02057f3a67aec395a2efd2e70426f144-911539ec-96815074"
+	// mailGun := email.NewMailgunService(mailGunAPIKey)
 
-	mailGun.SendEmail("Test Subject", "paul.borjesson.sesma3435@gmail.com", "This is a test email body.")
+	// mailGun.SendEmail("Test Subject", "paul.borjesson.sesma3435@gmail.com", "This is a test email body.")
 }
--- pkg/email/email.go
+++ pkg/email/email.go
@@ -2,5 +2,5 @@ package email
 
 //This interface will be implemented by all the email delivering systems, this allows for both resilience and easily cycling through them for fallbacks
 type EmailService interface {
-	SendEmail(subject, to, body string) error
+	SendEmail(subject, to, body string)
 }
